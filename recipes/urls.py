868bb53660dc827e58ffd779f19cdf06d2bc2a58
edinhoKgC <edilson.td@outlook.com>
2025-10-19 10:42:00 -0300

    .gitignore adicionado

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import home, sobre, contato
+from .views import home
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'ventilador': {'marca': 'LG', 'tamanho': 'grande'}})
-
-def sobre(request):
-    return HttpResponse("Sobre")
-
-def contato(request):
-    return HttpResponse("Contato")
+    
+    homecontext = {'ventilador': {
+                    'marca': 'LG', 
+                    'tamanho': 'grande'
+                    }
+               }
+    
+    return render(request, 'recipes/pages/home.html', context=homecontext, status=200)
